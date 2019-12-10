4ded07fd5569a496ffa45513fb2d5c06b108581d
jh1 <kar00034@naver.com>
2019-12-10 09:55:22 +0900

    test.ui 변경 main에서 기능 추가

--- hello_pyqt5.py
+++ hello_pyqt5.py
@@ -7,8 +7,17 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("hello_pyqt5.ui")
+        self.ui = uic.loadUi("chapter01/test.ui")
+        ok = self.ui.btn_ok.clicked.connect(self.showProp)
         self.ui.show()
+
+    def showProp(self):
+        print("red {} green {} blue {}".format(self.ui.spin_red.value(), self.ui.spin_green.value(),
+                                               self.ui.spin_blue.value()))
+        self.ui.spin_red.setValue(128)
+        self.ui.spin_green.setValue(128)
+        self.ui.spin_blue.setValue(128)
+        print(dir(self.ui.spin_red))
 
 
 if __name__ == "__main__":
